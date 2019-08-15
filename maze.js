c8fe6908fd0095b89c26d836da55a635dd27d6f8
Sami Morco <sami.morco@plated.com>
2019-08-15 12:56:29 -0400

    i stole code off the internets

--- maze.js
+++ maze.js
@@ -1,7 +1,85 @@
-console.log('hello');
-const canvas = document.getElementById('maze')
-const context = canvas.getContext("2d");
-context.fillStyle = 'blue';
-context.fillRect(250, 50, 100, 100);
-context.font = "12px Arial";
-context.fillText("Hello World",10,50);
+// JavaScript Document
+
+////// Variables //////
+var ctx = document.getElementById("ctx").getContext("2d");
+
+var canvas = { width: 300, height: 300 };
+var score = 0;
+
+var player = {
+  x: canvas.width / 2,
+  y: canvas.height - 100,
+  speed: 3
+};
+
+var LEFT = false;
+var RIGHT = false;
+var UP = false;
+var DOWN = false;
+
+
+////// Arrow keys //////
+
+function move() {
+
+  if (LEFT) {
+    player.x -= player.speed;
+  }
+  if (RIGHT) {
+    player.x += player.speed;
+  }
+
+  if (DOWN) {
+    player.y += player.speed;
+  }
+  if (UP) {
+    player.y -= player.speed;
+  }
+
+}
+
+document.onkeydown = function (e) {
+  if (e.keyCode == 37) LEFT = true;
+  if (e.keyCode == 38) UP = true;
+  if (e.keyCode == 39) RIGHT = true;
+  if (e.keyCode == 40) DOWN = true;
+}
+
+document.onkeyup = function (e) {
+  if (e.keyCode == 37) LEFT = false;
+  if (e.keyCode == 38) UP = false;
+  if (e.keyCode == 39) RIGHT = false;
+  if (e.keyCode == 40) DOWN = false;
+}
+
+
+////// other functions //////
+
+
+//function to clear canvas
+function clearCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+// Draw Player ship.
+function ship(x, y) {
+  var x = player.x;
+  var y = player.y;
+  ctx.fillStyle = "#FFFFFF";
+
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  ctx.lineTo(x + 15, y + 15);
+  ctx.lineTo(x - 15, y + 15);
+  ctx.fill();
+}
+
+// update
+
+setInterval(update, 10);
+
+function update() {
+  clearCanvas();
+  ship();
+  move();
+}
